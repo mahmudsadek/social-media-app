678f34791d5404b30eaf9937ffff2775ddd9f59d
sadek <mahmoudsadek.1905@gmail.com>
2024-04-27 22:08:36 +0300

    add the context

---
+++ social-media-app/DBContext/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using social_media_app.Models;
+
+namespace social_media_app.DBContext
+{
+    public class Context: IdentityDbContext<User>
+    {
+        public DbSet<User> User { get; set; }
+        
+        public DbSet<Post> Post { get; set; }
+
+        public DbSet<Comment> Comment { get; set; } 
+
+        public DbSet<React> React { get; set; }
+
+        public DbSet<Replay> Replay { get; set; }
+    }
+}
