9db6a50a380b366995455314d3f1ee6e0cdf276e
sadek <mahmoudsadek.1905@gmail.com>
2024-04-27 23:34:27 +0300

    edit context

--- social-media-app/DBContext/Context.cs
+++ social-media-app/DBContext/Context.cs
@@ -15,5 +15,18 @@ namespace social_media_app.DBContext
         public DbSet<React> React { get; set; }
 
         public DbSet<Replay> Replay { get; set; }
+        
+        public Context(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    //// for default data in database like this ^_^
+
+        //    //modelBuilder.Entity<Product>()
+        //    //    .HasData(new Product() { Id = 2, Name = "car", Description = "Expinsive one", Price = 20000, Quentity = 10 });
+
+        //}
     }
+
+    
 }
