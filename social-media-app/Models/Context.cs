d53305e008713b24e71f83ded7d455241d02e743
Anas <anasashrafelzayat@gmail.com>
2024-04-27 21:14:39 +0200

    HelloRepo

---
+++ social-media-app/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace social_media_app.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //// for default data in database like this ^_^
+
+            //modelBuilder.Entity<Product>()
+            //    .HasData(new Product() { Id = 2, Name = "car", Description = "Expinsive one", Price = 20000, Quentity = 10 });
+
+        }
+    }
+}
