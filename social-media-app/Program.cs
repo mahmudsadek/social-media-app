9798a9d63dafed236638a08e2ef191a76f1f7951
sadek <mahmoudsadek.1905@gmail.com>
2024-04-28 22:18:22 +0300

    remove weather

--- social-media-app/Program.cs
+++ social-media-app/Program.cs
@@ -21,7 +21,7 @@ namespace social_media_app
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-//            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<Context>(options =>
             {
