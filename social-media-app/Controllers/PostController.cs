cef24802ed8a31571a91dabc5f10cda40c222f65
Anas <anasashrafelzayat@gmail.com>
2024-04-29 00:25:41 +0200

    salma_update

--- social-media-app/Controllers/PostController.cs
+++ social-media-app/Controllers/PostController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using social_media_app.DTOs;
 using social_media_app.Models;
 using social_media_app.Repository;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace social_media_app.Controllers
 {
@@ -17,15 +20,24 @@ namespace social_media_app.Controllers
 
         // GET: api/posts
         [HttpGet]
-        public ActionResult<IEnumerable<Post>> GetPosts()
+        public ActionResult<IEnumerable<PostDTO>> GetPosts()
         {
-            var posts = _postRepository.GetAll();
+            var posts = _postRepository.GetAll()
+                .Select(post => new PostDTO
+                {
+                    Content = post.Content,
+                    PostTime = post.PostTime,
+                    PostImage = post.PostImage,
+                    UserId = post.UserId
+                })
+                .ToList();
+
             return Ok(posts);
         }
 
         // GET: api/posts/5
         [HttpGet("{id}")]
-        public ActionResult<Post> GetPost(int id)
+        public ActionResult<PostDTO> GetPost(int id)
         {
             var post = _postRepository.Get(id);
 
@@ -34,26 +46,49 @@ namespace social_media_app.Controllers
                 return NotFound();
             }
 
-            return Ok(post);
+            var postDTO = new PostDTO
+            {
+                Content = post.Content,
+                PostTime = post.PostTime,
+                PostImage = post.PostImage,
+                UserId = post.UserId
+            };
+
+            return Ok(postDTO);
         }
 
         // POST: api/posts
         [HttpPost]
-        public IActionResult CreatePost(Post post)
+        public IActionResult CreatePost(PostDTO postDTO)
         {
+            var post = new Post
+            {
+                Content = postDTO.Content,
+                PostTime = (DateTime)postDTO.PostTime,
+                PostImage = postDTO.PostImage,
+                UserId = postDTO.UserId
+            };
+
             _postRepository.Insert(post);
             _postRepository.Save();
-            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, postDTO);
         }
 
         // PUT: api/posts/5
         [HttpPut("{id}")]
-        public IActionResult UpdatePost(int id, Post post)
+        public IActionResult UpdatePost(int id, PostDTO postDTO)
         {
-            if (id != post.Id)
+            var post = _postRepository.Get(id);
+
+            if (post == null)
             {
-                return BadRequest();
+                return NotFound();
             }
+
+            post.Content = postDTO.Content;
+            post.PostTime = (DateTime)postDTO.PostTime;
+            post.PostImage = postDTO.PostImage;
+            post.UserId = postDTO.UserId;
 
             _postRepository.Update(post);
             _postRepository.Save();
---
+++ social-media-app/DTOs/PostDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using social_media_app.Repository;
+using System.Runtime.Intrinsics.X86;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace social_media_app.DTOs
+{
+    public class PostDTO
+    {
+
+        public string Content { get; set; }
+
+        public DateTime? PostTime { get; set; }
+
+        public string? PostImage { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
