4fd7e7c8c08e9847d72258619ba066e6bce7885c
reefaey <abdallah.hussien101@gmail.com>
2024-05-04 18:45:32 +0300

    Create Chat and Message Models

---
+++ social-media-app/Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace social_media_app.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int SenderId { get; set; }
+
+        [ForeignKey("User")]
+        public int ReceiverId { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
---
+++ social-media-app/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace social_media_app.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [ForeignKey("Chat")]
+        public int ChatId { get; set; }
+
+        public DateTime SenderMessageTime { get; set; }
+        public string SenderMessage { get; set; }
+
+        public DateTime ReceiverMessagesTime { get; set; }
+        public string ReceiverMessage { get; set; }
+
+        
+
+    }
+}
