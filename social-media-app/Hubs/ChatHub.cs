5002e2e304fce07b71ccd713947d43eb33f1fa96
reefaey <abdallah.hussien101@gmail.com>
2024-05-10 23:25:14 +0300

    Chat Groub

--- social-media-app/Hubs/ChatHub.cs
+++ social-media-app/Hubs/ChatHub.cs
@@ -20,5 +20,19 @@ namespace social_media_app.Hubs
         {
             return Clients.User(userId).SendAsync("ReceiveMessage", message);
         }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
     }
 }
